2e7f7a89a64f264a30e4dcee42b79390ba5222ee
seth skinner <sethwskinner@gmail.com>
2020-10-06 00:39:44 -0500

    building out API

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -10,6 +10,33 @@ export default {
 
         return axios.get('/api.books');
 
+    },
+
+    getBook: (id) => {
+
+        return axios.get('/api/books/' + id);
+
+    },
+
+    deleteBook: (id) => {
+
+        return axios.delete('/api/books/' + id);
+
+    },
+    
+    saveBook: (bookData) => {
+
+        console.log('bookData: ')
+        console.log(bookData)
+        return axios.post('/api/books', bookData);
+
+    },
+
+    searchBook: (bookData) => {
+
+        console.log(bookData)
+        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${bookData}&key=${API_KEY}`);
+
     }
 
-}
+};
