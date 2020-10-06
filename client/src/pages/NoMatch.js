7db08dae29005fb57c31f957bbf2e801af448f81
seth skinner <sethwskinner@gmail.com>
2020-10-06 00:52:34 -0500

    building out nomatch page

---
+++ client/src/pages/NoMatch.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Jumbotron from '../components/Jumbotron';
+
+NoMatch = () => {
+
+    return(
+
+        <Jumbotron>
+
+            <h1>
+
+                404 page not found
+
+            </h1>
+
+            <h1>
+
+                <span role='img' aria-label='fave with rolling eyes emoji'>
+
+                    🙄
+
+                </span>
+
+            </h1>
+
+        </Jumbotron>
+
+    );
+
+}
+
+export default NoMatch;
