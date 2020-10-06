c7304d3559564ed1d8d0432e916ba651dc32e245
seth skinner <sethwskinner@gmail.com>
2020-10-06 00:09:32 -0500

    adding book.js model

---
+++ models/book.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+
+    title: { type: [String], required: true },
+    author: { type: [String], required: true},
+    description: String,
+    href: String,
+    thumbnail: String,
+
+});
+
+const book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
