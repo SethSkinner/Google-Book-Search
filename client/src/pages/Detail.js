45264535e465c80a5c72dab501c6c590d3fe4685
seth skinner <sethwskinner@gmail.com>
2020-10-06 00:49:06 -0500

    building out detail page

---
+++ client/src/pages/Detail.js
@@ -0,0 +1,91 @@
+import { commands } from 'npm';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Col, Row, Container } from '../components/Grid';
+import Jumbotron from '../components/Jumbotron';
+import API from '../utils/API';
+
+class Deatail extends Component {
+
+    state = {
+
+        book: {}
+
+    };
+
+    componentDidMount() {
+
+        API.getBook(this.props.natch.params.id)
+        .then(res => this.setState({ book: res.data }))
+        .catch(err => console.log(err));
+
+    }
+
+    render() {
+
+        return(
+
+           <Container fluid>
+
+                <Row>
+
+                    <Col size='md-12'>
+
+                        <Jumbotron>
+
+                            <h1>
+
+                                {this.state.book.title} by {this.state.book.author}
+                            </h1>
+
+
+                        </Jumbotron>
+
+                    </Col>
+
+                </Row>
+
+                <Row>
+
+                    <Col size='md-10 md-offset-1'>
+
+                        <article>
+
+                            <h1>Synopsis</h1>
+
+                            <p>
+
+                                {this.state.book.synopsis}
+
+                            </p>
+
+                        </article>
+
+                    </Col>
+
+                </Row>
+
+                <Row>
+
+
+                    <Col size='md-2'>
+
+                        <Link to='/'>
+
+                            Back to Authors
+
+                        </Link>
+
+                    </Col>
+
+                </Row>
+
+           </Container> 
+
+        );
+
+    }
+
+}
+
+export default Detail;
